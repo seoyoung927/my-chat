72baee0e03658cad09c03bde46f646a2f7066fd4
seoyoung927 <seoyoung0000@naver.com>
2023-03-18 21:37:47 +0900

    edit

--- chat-front/src/pages/VideoPage.tsx
+++ chat-front/src/pages/VideoPage.tsx
@@ -19,6 +19,7 @@ function VideoPage() {
     const [mute, setMute] = useState(true);
     const [show, setShow] = useState(true);
     const [streams, setStreams] = useState<any[]>([]);
+
     const { roomId } = useParams();
     //recoil
     const chatRooms = useRecoilValue(chatRoomsState);
@@ -28,9 +29,11 @@ function VideoPage() {
     const myFace = useRef<HTMLVideoElement>(null);
     const peerFace = useRef<HTMLVideoElement>(null);
 
-    let myStream: any;
-    let myPeerConnection: any;
-
+    const [myStream, setMyStream] = useState<any>(null);
+    const [myPeerConnection,setMyPeerConnection] = useState<any>(new RTCPeerConnection());
+    //let myPeerConnection: any;
+    //let myStream: any;
+    
     async function getMedia(deviceId?: string) {
         const initialConstrains = {
             audio: true,
@@ -41,30 +44,28 @@ function VideoPage() {
             video: { deviceId: { exact: deviceId } },
         };
         try {
-            myStream = await navigator.mediaDevices.getUserMedia(
+            let myStream = await navigator.mediaDevices.getUserMedia(
                 deviceId ? cameraConstrains : initialConstrains
             );
             if (myFace.current) {
                 myFace.current.srcObject = myStream;
                 setStreams((prev) => [...prev, myStream]);
             }
+            return myStream;
         } catch (e) {
             console.log(e);
         }
     }
 
     async function initCall() {
-        await getMedia();
-        makeConnection();
-    }
-
-    async function initCall2() {
-        await initCall();
+        const result = await getMedia();
+        makeConnection(result);
+        setMyStream(result);
         await stompClient.send(`/welcome/${room.roomName}`, {}, JSON.stringify({"id":userName}));
     }
 
     useEffect(() => {
-        initCall2();
+        initCall();
         stompClient.connect({}, () => {
             stompClient.subscribe(`/topic/welcome/${room.roomName}`, async (data) => {
                 //console.log("data: ", JSON.parse(data.body));
@@ -105,19 +106,18 @@ function VideoPage() {
     }, []);
 
     // RTC Code
-    function makeConnection() {
-        myPeerConnection = new RTCPeerConnection();
+    function makeConnection(myStream:any) {
+        //let myPeerConnection = new RTCPeerConnection();
         myPeerConnection.addEventListener("icecandidate", handleIce);
         myPeerConnection.addEventListener("track", handleAddStream);
-        console.log(myStream.getTracks());
+        myPeerConnection.addEventListener("connectionstatechange", handleChange);
         myStream
             .getTracks()
             .forEach((track: any) => myPeerConnection.addTrack(track, myStream));
-
     }
 
     function handleIce(data: any) {
-        console.log("sent candidate: ", data.candidate);
+        //console.log("sent candidate: ", data.candidate);
         stompClient.send(`/ice/${room.roomName}`, {}, JSON.stringify({
             id: userName,
             ice: data.candidate
@@ -125,13 +125,22 @@ function VideoPage() {
     }
 
     function handleAddStream(data: any) {
-        console.log("final: ", data.streams[0],myStream);
+        //console.log("final: ", data.streams[0],myStream);
         if (peerFace.current) {
             setStreams((prev) => [...prev, data.streams[0]]);
             peerFace.current.srcObject = data.streams[0];
         }
     }
-    
+
+    function handleChange(data: any){
+        if(myPeerConnection.iceConnectionState==="disconnected"){
+            if(peerFace.current){
+                peerFace.current.srcObject = null;
+            }
+        }
+    }
+
+    //Mute and Show func
     function handleMute() {
         setMute(cur=>!cur);
         streams.forEach((stream:any)=>{
@@ -149,6 +158,75 @@ function VideoPage() {
             })
         });
     }
+    
+    function handleExit(){
+        // Get the local media stream and stop it
+        const localStream = myPeerConnection.getLocalStreams()[0];
+        
+        if (localStream) {
+            localStream.getTracks().forEach( (track:any) => track.stop() );
+        }
+
+        // Get the remote media stream and stop it
+        const remoteStream = myPeerConnection.getRemoteStreams()[0];
+        if (remoteStream) {
+            remoteStream.getTracks().forEach( (track:any) => track.stop() );
+        }
+
+        // Close the peer connection
+        myPeerConnection.close();
+
+        if (myFace.current) {
+            myFace.current.srcObject = null;
+        }
+        if (peerFace.current) {
+            peerFace.current.srcObject = null;
+        }
+    }
+
+    //render frame func     
+    const [data,setData] = useState<any>();
+    const [result,setResult] = useState(""); 
+    useEffect(() => {
+        console.log("setInterval initiation");
+
+        const postImg = async(data:any) => {
+            const formData = new FormData();
+            formData.append('file', data);
+
+            try {
+            const response = await fetch('http://127.0.0.1:5000/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    "data": data.replace("data:image/png;base64,", "")
+                }),
+            });
+            if (!response.ok) throw new Error('bad server condition');
+            return response.json();
+            } catch (e:any) {
+                console.error('postImg Error: ', e.message);
+                return false;
+            }
+        };
+
+        const extractFrames = () => {
+            if(myFace.current){
+                const canvas = document.createElement('canvas');
+                canvas.width = 400
+                canvas.height = 300;
+                const ctx = canvas.getContext('2d');
+                ctx?.drawImage(myFace.current, 0, 0, canvas.width, canvas.height);
+                setData(canvas.toDataURL("image/png"))
+                postImg(canvas.toDataURL("image/png")).then((res)=>{
+                    //console.log(res['class_name']);
+                    setResult(res['class_name']);
+                });
+            }
+        };
+
+        setInterval(extractFrames, 20000);
+    },[]);
 
     return (
         <Container>
@@ -158,9 +236,11 @@ function VideoPage() {
                     className="Face"
                     autoPlay
                     playsInline />
-
+                <img src={data} />
+                <p>현재 사용자는 눈을 {result}한 상태입니다.</p>
                 <button onClick={handleMute}>{mute ? "음소거" : "음소거 해제"}</button>
                 <button onClick={handleShow}>{show ? "화면송출 중지" : "화면송출"}</button>
+                <button onClick={handleExit}>통화종료</button>
             </div>
             <div className="wrapper" id="peer">
                 <video
@@ -174,3 +254,4 @@ function VideoPage() {
 }
 
 export default VideoPage;
+

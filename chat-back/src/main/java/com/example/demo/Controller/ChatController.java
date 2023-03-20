4c853619cefb401bc3b739235ba7adcb9b832cbe
seoyoung927 <seoyoung0000@naver.com>
2023-03-20 19:32:48 +0900

    edit: back

--- chat-back/src/main/java/com/example/demo/Controller/ChatController.java
+++ chat-back/src/main/java/com/example/demo/Controller/ChatController.java
@@ -6,7 +6,10 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class ChatController {
@@ -20,6 +23,32 @@ public class ChatController {
 
     @MessageMapping("/hello/{room}")
     public void broadCast(@DestinationVariable("room") String room, Message message){
-        template.convertAndSend("/topic/"+room,message);
+        template.convertAndSend("/topic/chat/"+room,message);
     }
+
+    @MessageMapping("/welcome/{room}")
+    public void welcome(@DestinationVariable("room") String room, String message){
+        template.convertAndSend("/topic/welcome/"+room, message);
+    }
+
+    @MessageMapping("/offer/{room}")
+    public void offer(@DestinationVariable("room") String room, String message){
+        template.convertAndSend("/topic/offer/"+room,message);
+    }
+
+    @MessageMapping("/answer/{room}")
+    public void answer(@DestinationVariable("room") String room, String message){
+        template.convertAndSend("/topic/answer/"+room,message);
+    }
+
+    @MessageMapping("/ice/{room}")
+    public void ice(@DestinationVariable("room") String room, String message){
+        template.convertAndSend("/topic/ice/"+room,message);
+    }
+
+    @MessageMapping("/exit/{room}")
+    public void exit(@DestinationVariable("room") String room, String message){
+        template.convertAndSend("/topic/exit/"+room,message);
+    }
+
 }
